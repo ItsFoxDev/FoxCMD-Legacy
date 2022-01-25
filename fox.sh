bb0053c633bf8a48d8e6e5fb05a8d836ccdfcf3e
ItsFoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-01-25 16:15:58 -0500

    ⬇️ Added install command

--- fox.sh
+++ fox.sh
@@ -1,3 +1,12 @@
 if [ "$1" == "info" ]; then
   echo "FoxCMD 2.0.1"
 fi
+if [ "$1" == "install" ]; then
+  if [ "$2" == "brew"]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  if [ "$2" == "spicetify"]; then
+    curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+    curl -fsSL https://raw.githubusercontent.com/CharlieS1103/spicetify-marketplace/main/install.sh | sh
+  fi
+fi
